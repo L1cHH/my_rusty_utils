88d4165ba51cd48c958a2b75a4a4904ad1ee3805
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2024-09-30 20:17:46 +0300

    I/O for future commands were added, also current path displaying added

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+use std::env::current_dir;
+use std::io;
+use std::io::Write;
+use std::path::{PathBuf};
+
 mod commands;
+
+
+
+pub fn display_curr_path() {
+    let curr_dir_path =
+        current_dir()
+            .unwrap_or_else(|_| String::from("undefined path???").into());
+
+    write!(io::stdout(), "{}>", curr_dir_path.as_path().display()).unwrap();
+    io::stdout().flush().unwrap();
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env::current_dir;
+use std::fmt::Display;
+use std::io;
+use std::io::{BufRead, Write};
+use my_core_utils::display_curr_path;
+
 fn main() {
-    println!("Hello, world!");
+
+    loop {
+        let stdin = io::stdin();
+        let mut stdin_lock = stdin.lock();
+
+        display_curr_path();
+
+        let mut output = String::new();
+
+        match stdin_lock.read_line(&mut output) {
+            Ok(_) => {
+                println!("{output}");
+            },
+            Err(read_error) => {
+                println!("Problem with reading input into buffer... Error: {read_error:?}")
+            }
+        }
+
+
+    }
 }
